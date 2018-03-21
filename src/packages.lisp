931a11ab3ec1696d5ada65d78f61979c876800d9
erikmcguire <20667428+erikmcguire@users.noreply.github.com>
2018-03-21 18:07:10 -0400

    Add globals file.

--- src/packages.lisp
+++ src/packages.lisp
@@ -6,6 +6,7 @@
 (in-package :shiritori)
 
 #|
+(load "/globals.lisp" :external-format :utf-8)
 (load "/web-gui.lisp" :external-format :utf-8)
 (load "/loader.lisp" :external-format :utf-8)
 (load "/converters.lisp" :external-format :utf-8)
