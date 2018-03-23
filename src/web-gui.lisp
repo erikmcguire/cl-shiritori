746896f735b3f122c448762bbcb36b416480c36f
erikmcguire <20667428+erikmcguire@users.noreply.github.com>
2018-03-23 17:43:34 -0400

    Edit menu string.

--- src/web-gui.lisp
+++ src/web-gui.lisp
@@ -74,18 +74,18 @@
         (:p :lang "ja" :style "margin: 0 auto; width: 40%; padding: 5px; border: 3px groove; font-family: georgia, sans-serif; font-size: 12;"
         "
            Welcome to a prototype Common Lisp implementation of the Japanese word-chaining game,
-           <a href=\"https://www.japantimes.co.jp/life/2017/01/16/language/shiritori-simple-game-thats-great-practicing-japanese-vocab/\">
+           <a href=https://www.japantimes.co.jp/life/2017/01/16/language/shiritori-simple-game-thats-great-practicing-japanese-vocab/>
            <i>shiritori</i></a>.
              <br><br>When prompted with a word, respond with a word whose first syllable matches the final syllable of the prompt.
              For instance, if prompted with む<b>し</b>, respond with something like <b>し</b>る.
-             <br><br>If the final grapheme of the reading is a <a href=\"https://en.wikipedia.org/wiki/Yōon\">small kana</a>,
+             <br><br>If the final grapheme of the reading is a <a href=https://en.wikipedia.org/wiki/Yōon>small kana</a>,
              such as with 反射, or its kana form はん<b>しゃ</b>, match the final two kana in your response (e.g., <b>しゃ</b>ちょう).
              <br><br>If the final grapheme is 'ー', such as with エレベータ<b>ー</b>, then match the kana before it (e.g., <b>タ</b>バコ).
              <br><br>The system can recognize <i>hiragana, katakana, kanji,</i> and <i>romaji</i> input.
              <br><br>You may set a time limit for each prompt-response pair, between 1-20 seconds (5s by default).
              <br><br>If enabled, after quitting to menu, you can find exported file(s) in your default lisp folder
              (e.g., 'C\:\\acl10.1express\\'), named output_[universal_time].txt, or in an existing directory path of your choosing.
-             <br><br>Vocabulary data, for now, is from Wiktionary's <a href=\"https://en.wiktionary.org/wiki/Appendix:JLPT\">lists</a>.
+             <br><br>Vocabulary data, for now, is from Wiktionary's <a href=https://en.wiktionary.org/wiki/Appendix:JLPT>lists</a>.
              <br><br>You may enter the path to a custom import file, also: by default, the .txt or .csv should be tab-delimited
               with two columns: kanji word form and kana form; you can also use comma-delimited (e.g., on a given line: 漢字,かんじ).")
        (:br)
